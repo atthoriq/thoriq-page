fbd98ba9d704e9802f234c17be6eccb4a2c4ccd8
atthoriq <thorinick@gmail.com>
2019-12-22 23:14:00 +0700

    update config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: `At Thoriq Page`,
+    description: `At Thoriq Personal Page. `,
     author: `@gatsbyjs`,
   },
   plugins: [
@@ -18,11 +18,11 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `thoriq-page`,
+        short_name: `thoriq-page`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#357376`,
+        theme_color: `#357376`,
         display: `minimal-ui`,
         //icon: `src/images/thoriq-icon.ico`, // This path is relative to the root of the site.
       },
